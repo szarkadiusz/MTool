3cb6b0496df8f5359e5f09462c027b1c6b3608b7
Arek Szymanski <arkadiuszsz@gmail.com>
2020-09-24 08:26:21 +0200

    Initial commit

---
+++ src/main/java/pl/MTool/MToolApplication.java
@@ -0,0 +1,13 @@
+package pl.MTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/MTool/MToolApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.MTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
