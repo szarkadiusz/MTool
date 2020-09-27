e24ab59a5a77ae235da5171dd5684d8e73235c2b
Arek Szymanski <arkadiuszsz@gmail.com>
2020-09-27 22:57:35 +0200

    fixed css

--- src/main/java/pl/MTool/MToolApplication.java
+++ src/main/java/pl/MTool/MToolApplication.java
@@ -2,10 +2,15 @@ package pl.MTool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MToolApplication {
-
+public class MToolApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder springApplicationBuilder){
+        return springApplicationBuilder.sources(MToolApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(MToolApplication.class, args);
     }
--- src/main/java/pl/MTool/ServiceController/Page_Controler.java
+++ src/main/java/pl/MTool/ServiceController/Page_Controler.java
@@ -2,10 +2,10 @@ package pl.MTool.ServiceController;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.pi4j.io.gpio.*;
 
 @Controller
 public class Page_Controler {
-
 
     @GetMapping("/")
     public String getIndexPage(){
